62d113876c88674165c73b5a18122e028b8b0542
MarlonDiaz2022 <100392114+MarlonDiaz2022@users.noreply.github.com>
2023-05-29 09:33:23 -0500

    primer commit

---
+++ Segundo_Parcial/src/test/java/com/sringboot/clienteapp/SegundoParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sringboot.clienteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
